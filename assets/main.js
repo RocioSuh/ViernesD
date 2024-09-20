189c18e38e293ae687a2b4df8120b44f8367e980
Rocio <Rociosuhring@gmail.com>
2024-09-20 19:56:26 -0300

    cambios en clase

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+alert("hola desde main")
+console.log("hola")
+console.error("error")
+let X = 10
+const Y = 5
+X = 15
+console.log(X,Y)
+// console.log(X+Y) se suman )?
+let nombre = "Jisung"
+let edad = "25"
+console.log(nombre)
+console.log("Tu nombre es: " + nombre + " y tu edad es: " + edad )
+console.log(`Tu nombre es: ${nombre} y tu edad es ${edad}`)
+prompt(`ingrese su nombre: `)
+const NU = prompt(`ingrese su nombre: `)
+console.log(NU)
