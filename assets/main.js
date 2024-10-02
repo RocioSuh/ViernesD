1afba7cad15762c3ba0f73acfe3385810ffb01ec
RocioSuh <rociosuhring@gmail.com>
2024-10-02 19:24:08 -0300

    Desafio2

--- assets/main.js
+++
@@ -1,6 +0,0 @@
-let nombreProducto = "cafe"
-let precioUnitario = 3500
-const cantidadDeseada = prompt(`Cuantas unidades de café deseas comprar?: `)
-let costoTotal = (precioUnitario*cantidadDeseada)
-alert("El costo total de tu compra es: $" + costoTotal )
-
